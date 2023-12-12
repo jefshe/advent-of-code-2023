dc40e24966f015831250a121c7f5d684552fbad5
Jeffrey Shen <jeffrey.shen@kauza.dk>
2023-12-12 11:56:32 +0100

    parse inputs

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,12 +1,18 @@
-const INPUT: [&str; 6] = [
-"Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53",
-"Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19",
-"Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1",
-"Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83",
-"Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36",
-"Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11",
-];
-
+mod consts;
 fn main() {
-    
+    let inputs: Vec<(Vec<usize>, Vec<usize>)> = consts::INPUT
+        .into_iter()
+        .map(|s| {
+            let l_r: Vec<&str> = s.split("|").collect();
+            let [a,b ]: [&str] = l_r[0..2] else {panic!()};
+            let winning: Vec<usize> = unsafe_parse(a, 2);
+            let numbers: Vec<usize> = unsafe_parse(b, 0);
+            (winning, numbers)
+        })
+        .collect();
+    println!("{:?}", inputs);
+}
+
+fn unsafe_parse(line:  &str, skip: usize) -> Vec<usize> {
+    line.split_whitespace().skip(skip).map(|str| str.parse().expect(&format!("could not parse: {str}"))).collect()
 }
