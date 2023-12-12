062b168c8174317b04a7ceda53db501161e609d1
Jeffrey Shen <jeffrey.shen@kauza.dk>
2023-12-12 13:14:20 +0100

    part 2

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,18 +1,49 @@
 mod consts;
+use std::collections::HashSet;
+type Game = (HashSet<usize>, Vec<usize>);
 fn main() {
-    let inputs: Vec<(Vec<usize>, Vec<usize>)> = consts::INPUT
+    let inputs: Vec<(HashSet<usize>, Vec<usize>)> = consts::BIG_INPUT
         .into_iter()
         .map(|s| {
             let l_r: Vec<&str> = s.split("|").collect();
             let [a,b ]: [&str] = l_r[0..2] else {panic!()};
-            let winning: Vec<usize> = unsafe_parse(a, 2);
+            let winning: HashSet<usize> = unsafe_parse(a, 2);
             let numbers: Vec<usize> = unsafe_parse(b, 0);
             (winning, numbers)
         })
         .collect();
-    println!("{:?}", inputs);
+    part_b(inputs)
 }
 
-fn unsafe_parse(line:  &str, skip: usize) -> Vec<usize> {
+fn part_b(games: Vec<Game>) {
+    let mut card_count: Vec<usize> = std::iter::repeat(1).take(games.len()).collect();
+    for (base_idx, g )in games.iter().enumerate() {
+        for extra_copies in 0..get_matches(g) {
+            let game_idx = base_idx + extra_copies + 1;
+            card_count[game_idx] += card_count[base_idx];
+        }
+    }
+    let total: usize = card_count.iter().sum();
+    println!("total: {total}", )
+
+}
+
+fn part_a(games: Vec<Game>) {
+    let mut points = 0;
+    for g in games {
+        let matches = get_matches(&g);
+        if matches > 0 {
+            points += 1 << (matches-1)
+        }
+    }
+    println!("points: {points}")
+}
+
+fn get_matches(game: &Game) -> usize {
+    let (win_set, nums) = game;
+    nums.iter().filter(|n| win_set.contains(n)).count()
+}
+
+fn unsafe_parse<B: FromIterator<usize>>(line:  &str, skip: usize) -> B {
     line.split_whitespace().skip(skip).map(|str| str.parse().expect(&format!("could not parse: {str}"))).collect()
 }
