d780480e6d016a541b476d6aa24bd0e4f2381c5c
Jeffrey Shen <jeffrey.shen@kauza.dk>
2023-12-12 14:23:48 +0100

    parsing hell

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,22 +1,65 @@
 #![feature(let_chains)]
 use itertools::Itertools;
 mod consts;
+mod rectangle;
+
+use rectangle::{Coord, Rectangle};
 
 fn main() {
     let inputs :Vec<Vec<char>> = consts::BIG_INPUT.iter().map(|line| line.chars().collect_vec()).collect();
-    for i in 0..inputs.len() {
-        for j in 0..inputs[0].len() {
-            if (is_symbol(inputs[i][j])) {
-                println!("{i}, {j}: {}", inputs[i][j]);
+    let mut symbol_coords : Vec<Coord> = Vec::new();
+    let mut numbers: Vec<(usize, Rectangle)> = Vec::new();
+    let mut partial_number: String = String::new();
+    for j in 0..inputs.len() {
+        for i in 0..inputs[0].len() {
+            let c = inputs[j][i];
+            if is_digit(c)  {
+                partial_number.push(c)
+            } 
+            
+            if partial_number.len() > 0 && (i == inputs[0].len() - 1 || !is_digit(c))  {
+                let end_coord = (if is_digit(c) { i } else { i - 1 }, j);
+                numbers.push(parse_num(&partial_number, end_coord));
+                partial_number.clear()
             }
 
-            
+            if is_symbol(c) {
+                symbol_coords.push((i ,j));
+            }
         }
     }
+    let parts: Vec<usize> = numbers
+        .iter()
+        .filter(|(n, area)| symbol_coords.iter().any(|sym| {
+            if area.is_inside(sym) {
+                println!("{sym:?} is inside {n} [{area:?}]")
+            }
+            area.is_inside(sym)
 
+            
+        }))
+        .map(|(n, _)|*n)
+        .collect();
+    let parts_sum: usize = parts.iter().sum();
+    println!("{numbers:?}");
+    println!("SUM: {parts_sum}");
+}
+
+fn parse_num(num_str: &str, end_coord: Coord) -> (usize, Rectangle) {
+    let (i,j) = end_coord;
+    let value = num_str.parse().unwrap();
+    let area = Rectangle::surround(((i + 1 - num_str.len()), j), num_str.len());
+    if value == 755 {
+        println!("{area:?}, {i}, {j}");
+    }
+    (value, area)
 }
 
 fn is_symbol(c: char) -> bool {
-    c != '.' && (c < '0'|| c > '9')
+    c != '.' && !is_digit(c)
+}
+
+fn is_digit(c: char) -> bool {
+    c >= '0'&& c <= '9'
 }
 
---
+++ day3/src/rectangle.rs
@@ -0,0 +1,33 @@
+use std::cmp::max;
+
+pub type Coord = (usize, usize);
+#[derive(Debug)]
+pub struct Rectangle {
+    pub x: i64,
+    pub y: i64,
+    pub width: usize,
+    pub height: usize
+}
+
+impl Rectangle {
+    pub fn surround(coord: Coord, width: usize) -> Rectangle {
+        let x = coord.0 as i64 - 1;
+        let y = coord.1 as i64 - 1;
+        Self::new(x, y, width + 2, 3)
+    }
+
+    pub fn new(x: i64, y: i64, width: usize, height: usize) -> Rectangle {
+        Rectangle {
+            x,
+            y,
+            width,
+            height
+        }
+    }
+    pub fn is_inside(&self, coord: &Coord) -> bool {
+        let x = coord.0 as i64;
+        let y = coord.1 as i64;
+        x >= self.x && y >= self.y && x <= (self.x + self.width as i64) && y <= (self.y + self.height as i64)
+    }
+
+}
