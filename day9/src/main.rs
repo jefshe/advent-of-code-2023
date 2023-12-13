34a74c0da076f4c4f4f386adaa15ef649eb7914d
Jeffrey Shen <jeffrey.shen@kauza.dk>
2023-12-13 11:31:43 +0100

    day9 part 2

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -1,12 +1,33 @@
 use std::collections::{HashMap, HashSet};
 use itertools::Itertools;
-use regex::Regex;
-
+use part_a::{part_a, Sequence};
 
 mod consts;
 mod part_a;
 
 fn main() {
-    let sequences: Vec<Vec<usize>> = consts::INPUT.lines().map(|l| l.split_whitespace().map(|c| c.parse().unwrap()).collect()).collect();
-    println!("{sequences:?}")
+    let sequences: Vec<Sequence> = consts::INPUT.lines().map(|l| l.split_whitespace().map(|c| c.parse().expect(c)).collect()).collect();
+    // part_a
+    // let total: i32 = sequences.into_iter().map(build_pyramid).map(part_a).sum();
+    let total: i32 = sequences.into_iter().map(build_pyramid).map(|p| prev_seq(p, 0)).sum();
+    println!("{total}")
+}
+
+fn build_pyramid(seq: Sequence) -> Vec<Sequence> {
+    let mut pyramid: Vec<Sequence> = vec![seq];
+    let mut curr = pyramid.last().unwrap();
+    while pyramid.last().unwrap().iter().any(|v| *v != 0) {
+        let next_seq = curr.iter().tuple_windows().map(|(a,b)| b -a).collect();
+        pyramid.push(next_seq);
+        curr = pyramid.last().unwrap();
+    }
+    pyramid
+}
+
+fn prev_seq(pyramid: Vec<Sequence>, level: usize) -> i32{
+    if level == pyramid.len() - 2 {
+        pyramid[level][0]
+    } else {
+        pyramid[level][0] - prev_seq(pyramid, level + 1)
+    }
 }
--- day9/src/part_a.rs
+++ day9/src/part_a.rs
@@ -1,4 +1,18 @@
+use std::collections::VecDeque;
+
+use itertools::Itertools;
 
 
-fn part_a() {
+pub type Sequence = Vec<i32>;
+pub fn part_a(pyramid: Vec<Sequence>) -> i32 {
+    next_seq(&pyramid, 0)
+}
+
+fn next_seq(pyramid: &Vec<Sequence>, level: usize) -> i32 {
+    if level == pyramid.len() - 2 {
+        pyramid[level][0]
+    } else {
+        let s = pyramid[level].last().unwrap();
+        s + next_seq(pyramid, level + 1)
+    }
 }
